c6124097d03c6d78cc1cede2352fc85db3fe5844
rogo! <r480github@gmail.com>
2025-09-19 20:00:21 -0400

    New Setting: Blob cloak

--- public/settings/setting.js
+++ public/settings/setting.js
@@ -177,3 +177,48 @@ function checkBrowser() {
   }
 }
 checkBrowser()
+function launchBlob() {
+  const currentSiteUrl = window.location.href + "?redirect=true";
+
+  const htmlContent = `
+    <html>
+      <head>
+        <title>Space</title>
+        <style>
+          body,
+          html {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+          }
+          iframe {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: none;
+          }
+        </style>
+      </head>
+      <body>
+        <iframe src="${currentSiteUrl}"></iframe>
+      </body>
+    </html>
+	`;
+
+  const blob = new Blob([htmlContent], {
+    type: "text/html",
+  });
+
+  const blobUrl = URL.createObjectURL(blob);
+
+  let newWindow = window.open(blobUrl);
+  if (newWindow) {
+    newWindow.onload = () => {
+      newWindow.document.title = "Galaxy";
+    };
+  }
+}
