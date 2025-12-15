a5460a088191672d667b6e3f7558275dfaaccfb0
rogo! <119364101+r480github@users.noreply.github.com>
2025-12-15 16:33:53 -0500

    update

--- public/global/js/script.js
+++ public/global/js/script.js
@@ -1,12 +1,12 @@
 document.getElementById("search").addEventListener("keypress", function (event) {
-  if (event.key === "Enter") { // Check if the "Enter" key was pressed
-    event.preventDefault(); // Optional: Prevent default behavior, if needed
+  if (event.key === "Enter") { 
+    event.preventDefault(); 
     document.getElementById("web").style.visibility = "visible";
     document.getElementById("controls").style.visibility = "visible";
   }
 });
 document.querySelector("select").addEventListener("change", function () {
-  const selectedValue = this.value; // Get the selected option's value
-  const searchInput = document.querySelector("#search"); // Get the input element
-  searchInput.setAttribute("data-service", selectedValue); // Set the data-service attribute
+  const selectedValue = this.value; 
+  const searchInput = document.querySelector("#search");
+  searchInput.setAttribute("data-service", selectedValue);
 });
