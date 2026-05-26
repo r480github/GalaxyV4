9242b7aec159050172fa1c8e9233474e5b1841c7
rogo! <r480github@gmail.com>
2026-05-25 22:38:29 -0400

    remove chemicaljs

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-import { ChemicalServer } from "chemicaljs";
 import express from "express";
+const app = express();
 
-const [app, listen] = new ChemicalServer({
-    default: "scramjet", 
-    uv: "true",
-    scramjet: "true",
-    rh: "false",
-});
 const port = process.env.PORT || 8080;
 
 // --- Allow Caddy to issue TLS for all domains ---
@@ -14,21 +8,19 @@ app.get("/allow", (req, res) => {
   res.sendStatus(200); // allow all domains
 });
 
-app.use(express.static("public", {
+app.use(
+  express.static("public", {
     index: "index.html",
-    extensions: ["html"]
-}));
+    extensions: ["html"],
+  }),
+);
 
-
-app.serveChemical();
-
-
-listen(port, () => {
-    console.log("░██████╗░░█████╗░██╗░░░░░░█████╗░██╗░░██╗██╗░░░██╗");
-    console.log("██╔════╝░██╔══██╗██║░░░░░██╔══██╗╚██╗██╔╝╚██╗░██╔╝");
-    console.log("██║░░╚██╗██╔══██║██║░░░░░██╔══██║░██╔██╗░░░╚██╔╝░░");
-    console.log("╚██████╔╝██║░░██║███████╗██║░░██║██╔╝╚██╗░░░██║░░░");
-    console.log("░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░");
-    console.log("is listening on:");
-    console.log(`http://localhost:${port}`);
+app.listen(port, () => {
+  console.log("░██████╗░░█████╗░██╗░░░░░░█████╗░██╗░░██╗██╗░░░██╗");
+  console.log("██╔════╝░██╔══██╗██║░░░░░██╔══██╗╚██╗██╔╝╚██╗░██╔╝");
+  console.log("██║░░╚██╗██╔══██║██║░░░░░██╔══██║░██╔██╗░░░╚██╔╝░░");
+  console.log("╚██████╔╝██║░░██║███████╗██║░░██║██╔╝╚██╗░░░██║░░░");
+  console.log("░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░");
+  console.log("is listening on:");
+  console.log(`http://localhost:${port}`);
 });
